22dfbbc27fbd00b50f54387a4db733a298be3958
Eaton Lee <914189426@qq.com>
2019-05-17 22:30:36 +0800

    Add files via upload

--- spider.py
+++ spider.py
@@ -5,7 +5,7 @@ import threading
 import douban
 import renren
 import steam
-
+import music
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--spider", type=str, default="renren",
@@ -21,12 +21,15 @@ def run():
         renren.main()
     elif args["spider"]=="steam":
         steam.main()
+    elif args["spider"]=="163music":
+        music.main()    
     elif args["spider"]=="all":
         threading.Thread(target=douban.main()).start()
         threading.Thread(target=renren.main()).start()
         threading.Thread(target=steam.main()).start()
+        threading.Thread(target=music.main()).start()
 def main():
-    schedule.every(3600*args["time"]).seconds.do(run)
+    schedule.every(10*args["time"]).seconds.do(run)
     while True:
         schedule.run_pending()
 
