dd2cefbbabc21e036d0493eda063435e985075ab
Eaton Lee <914189426@qq.com>
2020-02-01 19:10:52 +0800

    Update steam.py
    Add some agents

--- steam.py
+++ steam.py
@@ -5,25 +5,30 @@ import pandas as pd
 
 class steam(object):
     def __init__(self,url,file_name,discount):
-        self.url=url
-        self.file_name=file_name
-        self.discount=discount
-        self.game_url=[]
-        self.game_name=[]
-        self.game_price=[]
-        self.publish_date=[]
-        self.headers = {'User-agent':  
-                      'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0'}
+        self._url=url
+        self._fileName=file_name
+        self._discount=discount
+        self._gameUrl=[]
+        self._gameName=[]
+        self._gamePrice=[]
+        self._publishDate=[]
+        self._headers = [{'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0'},
+                        {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1'},
+                        {'User-agent': 'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11'},
+                        {'User-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)'},
+                        {'User-agent': 'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11'}]
 
     def catch_data(self):
-        i =1 
+        i =1
 
         while True:
-            url = self.url+str(i)
+            a = i%5
+            url = self._url+str(i)
             #print(url)
             s = requests.session() 
             s.keep_alive = False
-            response = s.get(url, headers=self.headers)
+            response = s.get(url, headers=self._headers[a])
+            print(self._headers[a])
             response.encoding = response.apparent_encoding
             response=response.text
 
@@ -34,21 +39,21 @@ class steam(object):
                 try:
                     name = content.find(class_="title").get_text()
                     date = content.find("div",class_="col search_released responsive_secondrow").get_text()
-                    if self.discount == True:
+                    if self._discount == True:
                         price= content.find("div",class_="col search_price discounted responsive_secondrow").get_text()
-                    elif self.discount == False:
+                    elif self._discount == False:
                         price= content.find("div",class_="col search_price responsive_secondrow").get_text()
                     #img_src = content.find("div",class_="col search_capsule").find('img').get("src")
                     href=content.get("href")
 
-                    self.game_url.append(href)
-                    self.game_name.append(name)
-                    self.game_price.append(price)
-                    self.publish_date.append(date)
+                    self._gameUrl.append(href)
+                    self._gameName.append(name)
+                    self._gamePrice.append(price)
+                    self._publishDate.append(date)
                 except:
                     print("something error!")
             
-            if len(self.game_url)>=100:
+            if len(self._gameUrl)>=100:
                 break
             i=i+1
 
@@ -56,11 +61,11 @@ class steam(object):
         self.catch_data()
         ticks = [time.time()]
         ticks = list(ticks)
-        ticks.extend([""]*(len(self.game_name)-len(ticks)))
+        ticks.extend([""]*(len(self._gameName)-len(ticks)))
 
-        test = pd.DataFrame({'time':ticks,'name':self.game_name,'price':self.game_price,'date':self.publish_date,'url':self.game_url})
+        test = pd.DataFrame({'time':ticks,'name':self._gameName,'price':self._gamePrice,'date':self._publishDate,'url':self._gameUrl})
         print(test)
-        path = './'+self.file_name+str(ticks[0])+'.csv'
+        path = './'+self._fileName+str(ticks[0])+'.csv'
         test.to_csv(path,encoding='utf-8-sig',index=False)
 
 def main():
